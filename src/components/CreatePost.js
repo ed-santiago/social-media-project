2c59f6f32b692d3bccc96ca8ad64127a705cb863
Edmond <edmond.santiago7@gmail.com>
2024-01-04 20:22:45 +1100

    add CSS rules to WritePost component

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -2,12 +2,14 @@ import React from "react";
 
 function CreatePost() {
   return (
-    <div>
-      <h4>Create a post</h4>
+    <div id="create_post">
+      <h3>Create a post</h3>
       <hr />
-      <textarea type='text' name='textareaPost' placeholder="What's on your mind, User?" rows="4" cols="50" />
-      <input type='text' name='inputURLPost' placeholder="import image URL"/>
-      <button>Post</button>
+      <form>
+        <textarea type='text' name='textareaPost' placeholder="What's on your mind, User?" rows="4" cols="50" />
+        <input type='text' name='inputURLPost' placeholder="import image URL" />
+        <button>Post</button>
+      </form>
     </div>
   );
 }
