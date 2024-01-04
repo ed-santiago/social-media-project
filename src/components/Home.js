8b2d5a81215e8154301108365542412c8b9ab7f2
Edmond <edmond.santiago7@gmail.com>
2024-01-04 20:55:57 +1100

    fetch data from server

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,14 @@ import CreatePost from "./CreatePost";
 function Home({ isLoggedIn }) {
   const [postsArray, setPostsArray] = useState([]);
 
+  useEffect(() => {
+    fetch("https://social-media-project-s52o.onrender.com/posts")
+      .then((res) => res.json())
+      .then((posts) => setPostsArray(posts))
+  }, [])
+
+  console.log(postsArray)
+
   if (!isLoggedIn) return <Redirect to="/login" />
 
   return (
