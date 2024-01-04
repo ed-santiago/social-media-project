a66ba6e35c579abb1e7f85341936e4cbb1d8639d
Edmond <edmond.santiago7@gmail.com>
2024-01-04 22:01:11 +1100

    add CSS rules to profilePicture and name

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
 
 function Posts({ profilePicture, name, post, media, likes, comments }) {
 
   return (
-    <div>
-      <div>
-        <img src={profilePicture} />
-        <p>{name}</p>
-        <button>x</button>
+    <div id="posts">
+      <div id="post_header">
+        <div id="pic_name">
+          <img id="profile_picture" src={profilePicture} />
+          <p>{name}</p>
+        </div>
+        <FontAwesomeIcon id="x_button" icon={faXmark}/>
       </div>
     </div>
   );
