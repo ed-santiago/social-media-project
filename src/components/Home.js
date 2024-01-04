d5f7dfe4e69cb37ee9ba58dfc0b086379f30b50e
Edmond <edmond.santiago7@gmail.com>
2024-01-04 20:43:18 +1100

    call useState and useEffect hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import CreatePost from "./CreatePost";
 
 function Home({ isLoggedIn }) {
+  const [postsArray, setPostsArray] = useState([]);
+
   if (!isLoggedIn) return <Redirect to="/login" />
 
   return (
