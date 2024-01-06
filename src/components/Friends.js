535cbf4089616ef8750562f6250148f4931469a6
Edmond <edmond.santiago7@gmail.com>
2024-01-06 16:44:55 +1100

    add CSS rules to Friends header component

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -5,7 +5,7 @@ function Friends() {
     <div id="friends">
       <div id="friends_header">
         <img src="https://t4.ftcdn.net/jpg/05/62/99/31/360_F_562993122_e7pGkeY8yMfXJcRmclsoIjtOoVDDgIlh.jpg" />
-        <h1>Edmond's Friend List</h1>
+        <h1 style={{fontSize: "48px"}}>Edmond's Friend List</h1>
       </div>
     </div>
   );
