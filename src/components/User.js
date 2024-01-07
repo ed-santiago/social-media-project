6b13cc36a1986bd0a11ac9679d386ce7a66d3d77
Edmond <edmond.santiago7@gmail.com>
2024-01-07 15:19:21 +1100

    add CSS rules to User component

--- src/components/User.js
+++ src/components/User.js
@@ -2,7 +2,24 @@ import React from "react";
 
 function User() {
   return (
-    <h1>Edmond</h1>
+    <div id="user">
+      <div id="user_header">
+        <img src="https://t4.ftcdn.net/jpg/05/62/99/31/360_F_562993122_e7pGkeY8yMfXJcRmclsoIjtOoVDDgIlh.jpg" />
+        <div>
+          <h1>Edmond Santiago</h1>
+          <div id="user_friend_amount">
+            <p><b>Friends:</b> 425</p>
+            <p id="user_edit">Edit</p>
+          </div>
+        </div>
+        
+      </div>
+      <div id="user_body">
+          <p><b>Date Of Birth:</b> 29/04/1999</p>
+          <p><b>Address:</b> 123 Street, Suburb, NSW 2000, Australia</p>
+          <p><b>Occupation:</b> Software Engineer at Google</p>
+        </div>
+    </div>
   );
 }
 
