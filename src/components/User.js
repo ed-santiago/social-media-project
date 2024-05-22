4fadf4773a819193353bcab87222eb6a0e98770a
Edmond <edmond.santiago7@gmail.com>
2024-05-22 19:28:30 +1000

    change name to user, create UserDetails component and separate code from User to UserDetails

--- src/components/User.js
+++ src/components/User.js
@@ -1,4 +1,5 @@
 import React from "react";
+import UserDetails from "./UserDetails";
 import "../css/user.css";
 
 function User() {
@@ -7,19 +8,15 @@ function User() {
       <div id="user_header">
         <img src="https://t4.ftcdn.net/jpg/05/62/99/31/360_F_562993122_e7pGkeY8yMfXJcRmclsoIjtOoVDDgIlh.jpg" />
         <div>
-          <h1>Edmond Santiago</h1>
+          <h1>User User</h1>
           <div id="user_friend_amount">
             <p><b>Friends:</b> 425</p>
             <p id="user_edit">Edit</p>
           </div>
         </div>
-        
       </div>
-      <div id="user_body">
-          <p><b>Date Of Birth:</b> 29/04/1999</p>
-          <p><b>Address:</b> 123 Street, Suburb, NSW 2000, Australia</p>
-          <p><b>Occupation:</b> Software Engineer at Google</p>
-        </div>
+
+      <UserDetails />
     </div>
   );
 }
---
+++ src/components/UserDetails.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function UserDetails() {
+  return (
+    <div id="user_body">
+      <p><b>Date Of Birth:</b> 29/04/1999</p>
+      <p><b>Address:</b> 123 Street, Suburb, NSW 2000, Australia</p>
+      <p><b>Occupation:</b> Software Engineer at Google</p>
+    </div>
+  );
+}
+
+export default UserDetails;
