0338d3f28c40ed65fd675e6be2d3687a7510eb1d
Edmond <edmond.santiago7@gmail.com>
2024-05-24 19:27:22 +1000

    adjust login css

--- src/components/UserEditDetails.js
+++ src/components/UserEditDetails.js
@@ -1,27 +1,30 @@
 import React from "react";
+import "../css/userEditDetails.css";
 
-function UserEditDetails({editProfile, onChange, onSubmit}) {
-  const {profilePicture, name, dob, address, occupation} = editProfile;
+function UserEditDetails({ editProfile, onChange, onSubmit }) {
+  const { profilePicture, name, dob, address, occupation } = editProfile;
 
   return (
-    <form onSubmit={onSubmit}>
-      <h2>Edit Profile</h2>
-      <button type="submit">Save</button>
+    <form id="edit_form" onSubmit={onSubmit}>
+      <div id="save_edit">
+        <h2>Edit Profile</h2>
+        <button id="save" type="submit">Save</button>
+      </div>
 
       <label htmlFor="profilePicture">Profile Picture: </label>
-      <input id="profilePicture" type="url" name="profilePicture" value={profilePicture} onChange={onChange} />
+      <input id="profilePicture" type="url" name="profilePicture" value={profilePicture} onChange={onChange} /><br />
 
       <label htmlFor="name">Name: </label>
-      <input id="name" type="text" name="name" value={name} onChange={onChange} />
+      <input id="name" type="text" name="name" value={name} onChange={onChange} /><br />
 
       <label htmlFor="dob">Date Of Birth: </label>
-      <input id="dob" type="date" name="dob" value={dob} onChange={onChange} />
+      <input id="dob" type="date" name="dob" value={dob} onChange={onChange} /><br />
 
       <label htmlFor="address">Address: </label>
-      <input id="address" type="text" name="address" value={address} onChange={onChange} />
+      <input id="address" type="text" name="address" value={address} onChange={onChange} /><br />
 
       <label htmlFor="occupation">Occupation: </label>
-      <input id="occupation" type="text" name="occupation" value={occupation} onChange={onChange} />
+      <input id="occupation" type="text" name="occupation" value={occupation} onChange={onChange} /><br />
     </form>
   );
 }
